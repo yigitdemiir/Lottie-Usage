0ebb882df4d5dfd5d6b622bb59a44fa39014ac49
Yiğit Demir <yigitdemir8@hotmail.com>
2023-10-13 15:40:58 +0300

    Lottie Usage
    Simple way to add a Lottie animation to a Flutter project

---
+++ lottie_usage/android/app/src/main/kotlin/com/example/lottie_usage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lottie_usage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lottie_usage/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Lottie.network(
+              'https://assets3.lottiefiles.com/packages/lf20_ydo1amjm.json'),
+        ),
+      ),
+    );
+  }
+}
